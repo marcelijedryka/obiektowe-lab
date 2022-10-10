3387680cb5040789a1bda0705029ccd6d6c81d70
marcelijedryka <mjedryka@student.agh.edu.pl>
2022-10-10 16:00:33 +0200

    lab1

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,41 +1,9 @@
 package agh.ics.oop;
-import java.util.Scanner;
 import static agh.ics.oop.Direction.ChoosePath;
 import static java.lang.System.out;
 
 public class World {
     static void run(ChoosePath[] array) {
-        //out.println("Zwierzak idzie do przodu"); punkt 8
-        // punkt 14
-       /* Scanner sc = new Scanner(System.in);
-        out.println("Wprowadź kierunek (f , b , r lub l)");
-        String direction = sc.nextLine();
-        String outcome = switch(direction) {
-            case "f" -> "idzie do przodu";
-            case "b" -> "idzie do tyłu";
-            case "l" -> "skręca w lewo";
-            case "r" -> "skęca w prawo";
-            default -> "idzie w nieznaną stronę - błędna komenda";
-        };
-        out.println("Zwierzak " + outcome); */
-        // punkt 15
-        /*for (int i = 0; i < array.length; i++) {
-            String outcome=switch (array[i]) {
-                case "f" -> "idzie do przodu";
-                case "b" -> "idzie do tyłu";
-                case "l" -> "skręca w lewo";
-                case "r" -> "skęca w prawo";
-                default -> "idzie w nieznaną stronę - błędna komenda";
-            };
-            out.println("Zwierzak " + outcome);
-        }*/
-
-
-        // punkt 11
-        /*for (int i = 0; i < array.length - 1; i++) {
-            out.print(array[i] + "," + " ");
-        }
-        out.println(array[array.length - 1]);*/
         for (int i = 0; i < array.length; i++) {
             String output = switch (array[i]) {
                 case FORWARD -> "Zwierzak idzie do przodu";
@@ -62,8 +30,6 @@ public class World {
 
     public static void main(String[] args) {
         out.println("system wystartował");
-        //String[] pets = {"dog", "cat", "mouse"}; punkt 11-12
-        //String[] test = {"f" ,"f" ,"l" ,"r"};
         ChoosePath[] path = FindPath(args);
         run(path);
         out.println("system zakończył działanie");
